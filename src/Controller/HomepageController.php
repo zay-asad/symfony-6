5c44991313877da903faf1d39b4455d6d5298741
Zayan Asad <zayan.asad@sky.uk>
2022-11-09 18:11:05 +0000

    created homepage

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/homepage', name: 'app_homepage')]
+
+    public function index(): Response
+    {
+        //storing in variable
+        $homepage = "Welcome to the homepage!!!!";
+
+        return $this->render('homepage.html.twig', [
+            'homepage' => $homepage
+        ]);
+    }
+}
